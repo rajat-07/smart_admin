798f994eb0841d3bf45ecbb90b9255f9a08757c5
rajat-07 <49556754+rajat-07@users.noreply.github.com>
2020-01-16 19:08:44 +0530

    Add files via upload

---
+++ src/databaseConnection.java
@@ -0,0 +1,24 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class databaseConnection {
+    
+    final static String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    final static String DB_URL = "jdbc:mysql://localhost:3306/student";
+    final static String USER = "root";
+    final static String PASS = "";
+    
+    public static Connection connection(){
+        try{
+            Class.forName(JDBC_DRIVER);
+            Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            return conn;
+        }catch(ClassNotFoundException | SQLException e){
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        }
+    }
+}
